249e82b1e203810c6b6b56918971da2bd28112ee
Daniel Demelash <danielddemissie@gmail.com>
2023-10-19 20:50:42 +0300

    update: reverse string

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,9 +1,18 @@
+// Global solution 
 extern crate unicode_segmentation;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
     let grapheme:Vec<&str> = UnicodeSegmentation::graphemes(input, true).collect();
     let reverse_grapheme:Vec<&str> = grapheme.into_iter().rev().collect();
-    
+
     reverse_grapheme.concat()
 }
+
+
+/*  
+* simple solution
+pub fn reverse(input: &str) -> String{
+    input.chars().rev().collect()
+}
+*/
